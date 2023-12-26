9caca40b0010474d239c45ffebcf3cea45bdbb42
Shnappy <shnappey@gmail.com>
2023-12-26 18:16:49 +0000

    add inventory sorting and updating

--- Assets/Scripts/UnitControl/Unit.cs
+++ Assets/Scripts/UnitControl/Unit.cs
@@ -1,14 +1,30 @@
+using System;
 using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    public Inventory inventory;
     void Start()
     {
         UnitSelections.Instance.unitList.Add(this.gameObject);
     }
 
+    public void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponent<ItemObject>();
+        if (item)
+        {
+            inventory.AddItem(item.item, 1);
+            Destroy(other.gameObject);
+        }
+    }
     private void OnDestroy()
     {
         UnitSelections.Instance.unitList.Remove(this.gameObject);
+    }
+
+    private void OnApplicationQuit()
+    {
+        inventory.Container.Clear();
     }
 }
